7d37d57b225a9e7d8e5263f74ed878d07c40738f
Joseph Ko <eaglclaws@gmail.com>
2021-05-15 22:37:45 +0900

    fixed typo in status.sh, add urxvt setting is Xresources

--- bin/status.sh
+++ bin/status.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 hanweeks=("日" "月" "火" "水" "木" "金" "土")
 while true; do
-	xsetroot -name "[ enp3s0 | $(ip -4 addr show enp3s0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}') ][ $(date +"%H:%M %y/%m/%d (${hanweeks[$(date +w)]})") ]"
+	xsetroot -name "[ enp3s0 | $(ip -4 addr show enp3s0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}') ][ $(date +"%H:%M %y/%m/%d (${hanweeks[$(date +%w)]})") ]"
 	sleep 60
 done
